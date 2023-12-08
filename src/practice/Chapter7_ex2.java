2e7cfa7863e70e0eed88394753a53eb485c18393
MisuzuDate <138319041+maruri6797@users.noreply.github.com>
2023-12-08 13:55:27 +0900

    Chapter7完了

---
+++ src/practice/Chapter7_ex2.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Chapter7_ex2 {
+	public static void main(String[] args){
+		for (int i = 0; i < 10; i++) {
+			Thread t = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					System.out.println("無名クラスのテスト");
+				}
+			});
+			t.start();
+		}
+	}
+}
